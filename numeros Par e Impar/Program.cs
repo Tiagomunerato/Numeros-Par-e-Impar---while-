34f44b4683a1ba94ba49b50a9289c78a0863dafc
Tiagomunerato <tiagomunerato@gmail.com>
2019-10-05 10:17:23 -0300

    Add project files.

---
+++ numeros Par e Impar/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numeros_Par_e_Impar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, sp, si, cont;
+            sp = 0;
+            si = 0;
+            cont = 1;
+
+
+            while (cont <= 3)
+            {
+                Console.WriteLine("INFORME O Nº: ");
+                num = int.Parse(Console.ReadLine());
+
+                if (num % 2 == 0) { 
+                {
+                    sp = sp + num;
+                }
+                else
+                {
+                    si = si + num;
+                }
+
+                cont = cont + 1;
+            }
+            Console.WriteLine("SOMA PAR:", sp);
+            Console.WriteLine("SOMA IMPAR:", si);
+            Console.ReadKey();
+
+        }
+    }
+}
+
+
